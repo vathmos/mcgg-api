86e27acda081021692cff0d130423d47c7f22136
vathmos <esqeue@gmail.com>
2025-08-15 22:21:52 +0700

    add simple caching with Cache-Control headers

--- src/routes/commander.route.ts
+++ src/routes/commander.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getAllCommanders, getCommanderByParam } from "../controllers/commander.controller";
+import { cacheControl } from "../middleware/cacheControl";
 
 const router = Router();
 
-router.get("/commanders", getAllCommanders);
+router.get("/commanders", cacheControl(600),getAllCommanders);
 
-router.get("/commanders/:param", getCommanderByParam);
+router.get("/commanders/:param", cacheControl(600),getCommanderByParam);
 
 export default router;
